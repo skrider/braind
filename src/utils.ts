b0543fa969a56924eafe7f75f455e2b887dac6e0
skrider <skrider@berkeley.edu>
2022-10-13 15:59:12 -0700

    add createHash function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { createHash } from 'crypto'
 import { Readable, Writable } from 'stream'
 
 export class ReadableString extends Readable {
@@ -28,4 +29,11 @@ export class WritableString extends Writable {
   toString (): string {
     return this.str
   }
+}
+
+export function sha1 (string: string): string {
+  const input = new ReadableString(string)
+  const hash = createHash('sha1')
+  hash.push(input)
+  return hash.digest('base64')
 }
